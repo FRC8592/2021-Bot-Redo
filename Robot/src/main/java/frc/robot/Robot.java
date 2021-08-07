9a7031a6a2d663b354d82d6d645e90fa5da12309
Dillon Winchell <dillonwinchell@gmail.com>
2021-08-07 12:59:00 -0400

    Removed old implementations from Robot

--- Robot/src/main/java/frc/robot/Robot.java
+++ Robot/src/main/java/frc/robot/Robot.java
@@ -5,35 +5,15 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends TimedRobot {
   
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
-
-  TalonFX leftFront= new TalonFX(0); 
-  TalonFX leftBack = new TalonFX(0);
-  TalonFX rightFront = new TalonFX(0);
-  TalonFX rightBack = new TalonFX(0);
-
-  private final Joystick joystick = new Joystick(0);
-
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
   }
 
   /**
@@ -46,35 +26,13 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {}
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
   }
 
   /** This function is called once when teleop is enabled. */
@@ -83,32 +41,7 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {
-    rightBack.setInverted(true);
-    rightFront.setInverted(true);
-
-    
-
-    if (joystick.getRawAxis(0) > 0) {
-
-      leftFront.set(ControlMode.PercentOutput, 1);
-      leftBack.set(ControlMode.PercentOutput, 1);
-      rightFront.set(ControlMode.PercentOutput, 1);
-      rightBack.set(ControlMode.PercentOutput, 1);
-
-    }
-
-    else if (joystick.getRawAxis(0) < 0) {
-
-      leftFront.set(ControlMode.PercentOutput, -1);
-      leftBack.set(ControlMode.PercentOutput, -1);
-      rightFront.set(ControlMode.PercentOutput, -1);
-      rightBack.set(ControlMode.PercentOutput, -1);
-
-    }
-
-    else {}
-
+  public void teleopPeriodic() { 
   }
 
   /** This function is called once when the robot is disabled. */
