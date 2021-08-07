c9291b3d35c10b697e0408bd34f796d41813ffee
Dillon Winchell <dillonwinchell@gmail.com>
2021-08-07 12:30:58 -0400

    Stubbed TeleopController, DriveTrain and DriveControls classes

---
+++ Robot/src/main/java/frc/robot/TeleopController.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class TeleopController {
+
+    public DriveTrainState control(DriveControlsState driveControlsState, DriveTrainState driveTrainState) {
+        return null;
+    }
+    
+}
