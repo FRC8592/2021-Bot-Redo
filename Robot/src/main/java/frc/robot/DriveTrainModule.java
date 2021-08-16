8e9e977abb40c2101beebb3c336ffc9e5791d6b2
Dillon Winchell <dillonwinchell@gmail.com>
2021-08-15 20:57:49 -0400

    Moved DifferentialDrive code into DriveTrainModule

--- Robot/src/main/java/frc/robot/DriveTrainModule.java
+++ Robot/src/main/java/frc/robot/DriveTrainModule.java
@@ -1,11 +1,26 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 /**
  * Represents the motors used to drive the robot. We read the state of the module at the
  * beginning of each robot cycle. Then we update it using a controller. Then we write
  * the new, desired state back to the module. We also send the updated state as telemetry.
  */
 public class DriveTrainModule {
+
+	// TODO: replace these with the correct CAN bus numbers.
+  WPI_TalonFX leftFront= new WPI_TalonFX(0); 
+  WPI_TalonFX leftBack = new WPI_TalonFX(1);
+  WPI_TalonFX rightFront= new WPI_TalonFX(2); 
+	WPI_TalonFX rightBack = new WPI_TalonFX(3);
+
+  SpeedControllerGroup leftDrive = new SpeedControllerGroup(leftFront, leftBack);
+  SpeedControllerGroup rightDrive = new SpeedControllerGroup(rightFront, rightBack);
+  DifferentialDrive robotDrive = new DifferentialDrive(rightDrive, leftDrive);
 
 	/**
 	 * Read the current state from the hardware, like the current speed. This information
@@ -20,7 +35,9 @@ public class DriveTrainModule {
 	 * The vendor APIs for the hardware will not let us update some values, like current speed.
 	 */
 	public void writeState(DriveTrainState driveTrainState) {
-		// TODO: set the target percent output of the Falcon motors
+		double desiredSpeed = driveTrainState.getDesiredSpeed();
+		double desiredRotation = driveTrainState.getDesiredRotation();
+    robotDrive.arcadeDrive(desiredSpeed, desiredRotation);
 	}
     
 }
