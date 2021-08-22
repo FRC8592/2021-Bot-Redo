1b8c30ba2feeafda3834305410ce09e784d9afab
Dillon Winchell <dillonwinchell@gmail.com>
2021-08-22 18:20:26 -0400

    Pulled code from hardeware_test into DriveControlsModule

--- Robot/src/main/java/frc/robot/DriveControlsModule.java
+++ Robot/src/main/java/frc/robot/DriveControlsModule.java
@@ -15,8 +15,61 @@ import edu.wpi.first.wpilibj.Joystick;
  */
 public class DriveControlsModule {
 
-	// We are chosing to use a joystick for the drive controls, for now.
-	private Joystick joystick = new Joystick(0);
+	//
+    // Define joystick names.  Each name must have an associated USB ID.
+    //
+    private static final int DRIVE_STICK = 0;                    // Define USB IDs for joysticks
+    private static final int TURRET_STICK = 1;                    // USB IDs must start with 0
+    private static final int[] JOYSTICKS = {DRIVE_STICK, TURRET_STICK};
+
+    //
+    // Define the control scheme below.  Each named control is defined by a
+    // two-element array containing the joystick ID and the button or axis ID
+    //
+
+    // Drivetrain control
+    private static final int[] FORWARD_REV_AXIS  = {DRIVE_STICK,1};
+    private static final int[] TURN_AXIS        = {DRIVE_STICK,2};
+
+    // Collector control
+    private static final int[] COLLECT_DOWN_BUTTON  = {DRIVE_STICK,1};
+    private static final int[] COLLECT_UP_BOTTON    = {DRIVE_STICK,2};
+    private static final int[] DRIVER_UNJAM_BUTTON  = {DRIVE_STICK,3};
+
+    // Turret control
+    private static final int[] FLYWHEEL_START_BUTTON = {TURRET_STICK,1};
+    private static final int[] FLYWHEEL_STOP_BUTTON  = {TURRET_STICK,1};
+    private static final int[] FLYWHEEL_SPEED_AXIS   = {TURRET_STICK,3};   // Launch speed is adjustable
+    private static final int[] LAUNCH_BUTTON        = {TURRET_STICK,1};
+    private static final int[] TURRET_UNJAM_BUTTON   = {TURRET_STICK,2};
+    private static final int[] TURRET_AXIS      = {TURRET_STICK,2};
+
+    // Array to hold joystick objects
+    private Joystick[] m_joysticks = new Joystick[JOYSTICKS.length];
+
+    //
+    // Create joystick objects from the definitions above
+    //
+    public DriveControlsModule() {
+        for (int i = 0; i < JOYSTICKS.length; i++) {
+            m_joysticks[JOYSTICKS[i]] = new Joystick(JOYSTICKS[i]);
+        }
+    }
+
+    //
+    // Get status for a button
+    //
+    private boolean getButton(int[] buttonConstant) {
+        return m_joysticks[buttonConstant[0]].getRawButton(buttonConstant[1]);
+    }
+
+
+    //
+    // Get value for a joystick axis
+    //
+    private double getAxis(int[] axisConstant) {
+        return m_joysticks[axisConstant[0]].getRawAxis(axisConstant[1]);
+    }
 
 	/**
 	 * Read the state of the hardware and return a DriveControlsState containing that information.
@@ -28,8 +81,8 @@ public class DriveControlsModule {
 	public DriveControlsState readState() {
 		DriveControlsState state = new DriveControlsState();
 
-		state.setForwardBackward(joystick.getRawAxis(1)); // Y axis
-		state.setLeftRight(joystick.getRawAxis(0)); //idk what the x axis of the joystick is but i think its 0. -Zolton
+		state.setForwardBackward(getAxis(FORWARD_REV_AXIS));
+		state.setLeftRight(getAxis(TURN_AXIS));
 
 		return state;
 	}
