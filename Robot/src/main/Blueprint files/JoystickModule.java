e0defb3cc44f10f8b8db6996d043f0b35015441b
bob <ols.hedgehog@gmail.com>
2021-08-29 20:26:50 -0400

    This adds the Joysticks to the Hardware Constants and Adds the Joystick Modules

---
+++ Robot/src/main/Blueprint files/JoystickModule.java
@@ -0,0 +1,26 @@
+import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.HardwareIds;
+/**
+ * @author Alex Shcherbina
+ * Joystick input module
+ */
+public class JoystickModule implements RobotModule{
+    private Joystick driveStick;   
+    private Joystick turretStick; 
+    public JoystickModule(){
+        this.driveStick = new Joystick(HardwareConstants.DRIVE_STICK);
+        this.turretStick = new Joystick(HardwareConstants.TURRET_STICK);
+    }
+    
+
+     /*** This is the entry point int module use this module to read data from sensors or make calculations, or output commands 
+     * to robot hardware if you do this write only on the update should be exposed for a module. all other methods shoud be private
+     * with the exception of the getState method below. update will be called once per module by teleop Periodic
+     */
+    public void update();
+
+    /** Coach Dillan Recomends state Obects or hashmaps are usefull and Coach Sam agrees With him states and hashmaps are useful this will be the function
+    called to get this map from your object by the telemetry module you need to implement this for every Module you create*/
+    public Map<String, Double> getState();   
+}
+}
--- Robot/src/main/Blueprint files/SystemModule.java
+++ Robot/src/main/Blueprint files/SystemModule.java
@@ -1,9 +1,12 @@
 import java.util.HashMap;
 
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
 /** the Systems Module holds all constants needed globally for the program still 
  * technically a module just so a state can be written out to smart dashboard */
-public class HardwareConstants implements Module{
-
+public class HardwareConstants implements RobotModule{
+    public static final int DRIVE_STICK = 0;
+    public static final int TURRET_STICK = 1;
 
         /* hardware Module is acting a header or reference everything is public in this file I know this is not normal but all we are doing is assigning constants and pinouts
         everything is also public static as this will save time later on this module should only hold hardware constants. 
